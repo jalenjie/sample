159282273c973a4982bcb86cbc67da4d0bf7c395
jalen <jalen.0183@gmail.com>
2018-03-06 15:18:47 +0000

    fix

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,6 @@
 |
 */
 
-<<<<<<< HEAD
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
-=======
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
-
-Route::get("/", 'StaticPagesController@home');
-Route::get("/help", 'StaticPagesController@help');
-Route::get("/about", 'StaticPagesController@about');
->>>>>>> static-pages
