dbcf105baa4f9725dac22c31727a8d71af2968f3
jalen <jalen.0183@gmail.com>
2018-03-13 01:56:35 +0800

    Add password resets & email confirguration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPassword;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -32,7 +33,7 @@ class User extends Authenticatable
     {
         parent::boot();
 
-        static::creating(function ($user){
+        static::creating(function ($user) {
             $user->activation_token = str_random(30);
         });
     }
@@ -41,5 +42,10 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+    public function sendPasswordResetNotification ($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 }
